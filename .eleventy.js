86765f06985007c5462ec9ac962d05b06f79ce90
Lance Chen <hello@lancechen.tw>
2021-08-01 23:11:34 +0800

    Fix global data name

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
     notionToken: process.env.NOTION_TOKEN,
     pages: { indexPage: { id: "1e1420f1b2994070bdec65bc7c838fde" } },
     databases: {
-      articleDatabase: {
+      guideDatabase: {
         id: "10b45c7b7eac44bb8d7fd8bb12deb998",
       },
     },
